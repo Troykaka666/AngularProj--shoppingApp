be27bbcaeeed1688ddc976a90c1718ec975015a7
Troy <Troywu9527@gmail.com>
2018-12-28 23:14:37 -0500

    display the reciple while click the list item, and allow the user to add ingredients to the shopping list

--- src/app/recepies/recepies-list/recepies-list.component.ts
+++ src/app/recepies/recepies-list/recepies-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,12 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recepies-list.component.css']
 })
 export class RecepiesListComponent implements OnInit {
+@Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes:Recipe[] = [
-    new Recipe('Testing','Simply a test','https://i.kinja-img.com/gawker-media/image/upload/s--m6hjGHRf--/c_scale,f_auto,fl_progressive,q_80,w_800/aapfqrjiromx7x1m6rgy.jpg')
+    new Recipe('Testing','Simply a test','https://i.kinja-img.com/gawker-media/image/upload/s--m6hjGHRf--/c_scale,f_auto,fl_progressive,q_80,w_800/aapfqrjiromx7x1m6rgy.jpg'),
+    new Recipe('hahahaha','Simply a teasdf ads st','https://i.kinja-img.com/gawker-media/image/upload/s--m6hjGHRf--/c_scale,f_auto,fl_progressive,q_80,w_800/aapfqrjiromx7x1m6rgy.jpg')
   ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+  this.recipeWasSelected.emit(recipe);
   }
 
 }
